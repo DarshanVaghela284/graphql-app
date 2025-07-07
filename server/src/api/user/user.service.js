7a9ce1292d2513e22346f6d94ba4985f2f77f4c4
Darshan Vaghela <vdarshan613@gmail.com>
2025-07-07 18:49:59 +0530

    secure Authentication added

--- server/src/api/user/user.service.js
+++ server/src/api/user/user.service.js
@@ -3,32 +3,39 @@ import { signToken } from "../../utils/authentication.js";
 import { checkUserWithMobile } from "./user.dao.js";
 
 export async function loginUser({ mobile, password }) {
-  const user = await checkUserWithMobile({ mobile });
+  try {
+    const user = await checkUserWithMobile({ mobile });
 
-  if (!user) {
-    return Format.notFound(null, "User not registered with this mobile!");
-  }
+    if (!user) {
+      return Format.notFound(null, "User not registered with this mobile!");
+    }
 
-  const passwordMatch = password?.trim() === user.password;
-  if (!passwordMatch) {
-    return Format.forbidden(null, "Incorrect password!");
-  }
+    const passwordMatch = password?.trim() === user.password;
+    if (!passwordMatch) {
+      return Format.forbidden(null, "Incorrect password!");
+    }
 
-  if (!user.is_active) {
-    return Format.forbidden(
-      null,
-      "Please contact our help-desk to activate your account!"
+    if (!user.is_active) {
+      return Format.forbidden(
+        null,
+        "Please contact our help-desk to activate your account!"
+      );
+    }
+
+    const tokenPayload = {
+      first_name: user.first_name,
+      last_name: user.last_name,
+      mobile: user.mobile,
+      email: user?.email || null,
+    };
+
+    const token = signToken(tokenPayload);
+
+    return Format.success({ user, token }, "Login Successful");
+  } catch (error) {
+    throw Format.internalError(
+      error,
+      `Error in loginUser service: ${error.message || error}`
     );
   }
-
-  const tokenPayload = {
-    first_name: user.first_name,
-    last_name: user.last_name,
-    mobile: user.mobile,
-    email: user?.email || null,
-  };
-
-  const token = signToken(tokenPayload);
-
-  return Format.success({ user, token }, "Login Successful");
 }
